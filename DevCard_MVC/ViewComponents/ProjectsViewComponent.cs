d658fc5b05b054f4c6a9eedd7ae9e49cd0845bfc
Mohammad Sahari <mohammadsahari98@outlook.com>
2024-10-19 22:47:04 +0330

    component view feature created

---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+	public class ProjectsViewComponent : ViewComponent
+	{
+		public IViewComponentResult invoke()
+		{
+			return View("_Projects");
+		}
+	}
+}
--- DevCard_MVC/obj/Debug/net8.0/DevCard_MVC.AssemblyInfo.cs
+++ DevCard_MVC/obj/Debug/net8.0/DevCard_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevCard_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f9fba02be9e38f2163dd293a7680514c31f3ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57986d4a82c31c64ed3b23adefdcab0def35f21d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevCard_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevCard_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
